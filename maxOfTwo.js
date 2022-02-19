e5e5c9b64c0713e3262cd0c58396b000d2f70404
Travis Bee <99930434+tbee10@users.noreply.github.com>
2022-02-19 15:32:40 -0700

    Add files via upload

---
+++ maxOfTwo.js
@@ -0,0 +1,14 @@
+let x = 1
+let y = 2
+
+if (x > y) {
+    console.log("1");
+} else {
+    console.log("2");
+}
+if (x == y){
+    console.log("the values are identical");
+}
+
+// Write your code below this line.
+
